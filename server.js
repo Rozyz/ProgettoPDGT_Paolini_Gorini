dec7b0679f70ddab74760059f50daf5aac384b6c
Rozyz <paolettopaolini@yahoo.it>
2019-03-21 12:25:16 +0100

    Prime query e pulizia codice

--- server.js
+++ server.js
@@ -14,6 +14,24 @@ firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: "https://fuel-stations-italy.firebaseio.com"
 });
+
+var db = firebase.database();
+
+var ref = db.ref();
+ref.orderByChild("ccomune").equalTo("Lanciano").on("child_added", function(data) {
+  console.log(data.val().ccomune);
+})
+
+/*
+ref.on("value",function(snapshot){
+  console.log(snapshot.val());
+}, function(errorObject){
+  console.log("The read failed: " + errorObject.code);
+})
+
+console.log("Hello")
+*/
+
 /*
 const pool = mysql.createPool({
     connectionLimit: 10,
@@ -24,32 +42,13 @@ const pool = mysql.createPool({
 })
 */
 
-var db = firebase.database();
-var ref = db.ref("restricted_access/secret_document");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-});
-
-var usersRef = ref.child("users");
-usersRef.set({
-  alanisawesome: {
-    date_of_birth: "June 23, 1912",
-    full_name: "Alan Turing"
-  },
-  gracehop: {
-    date_of_birth: "December 9, 1906",
-    full_name: "Grace Hopper"
-  }
-});
-
-
 app.get("/", (req, res) => {
   /*console.log("Responding to root route") */
   res.send("Fuel stations")
 })
 
 
-const PORT = process.env.PORT || 3005
+const PORT = process.env.PORT || 3004
 // localhost:PORT
 app.listen(PORT, () => {
   console.log("Il server è online e ascolta sulla porta: " + PORT)
