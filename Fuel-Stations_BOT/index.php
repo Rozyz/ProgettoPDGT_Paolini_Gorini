caee4dfdf1b845941c91bb1c85a6ac47815e0994
Rozyz <paolettopaolini@yahoo.it>
2019-03-27 12:20:32 +0100

    Procfile aggiornato

--- Fuel-Stations_BOT/index.php
+++ Fuel-Stations_BOT/index.php
@@ -4,11 +4,13 @@
   $website = "https://api.telegram.org/bot".$botToken;
 
   $update = file_get_contents($website."/getupdates");
-  $updateArray = json_decode($update, TRUE);
+  $dati = json_decode($update, TRUE);
 
-  print_r($updateArray);
+  print_r($dati);
 
   $chatId = $updateArray['message']['from']['id'];
   $name = $updateArray['message']['from']['first_name'];
   $text = $updateArray['message']['text'];
+
+  http_request("https://api.telegram.org/bot{$botToken}/sendMessage?chat_id=".$chatId."&text=ciao");
  ?>
