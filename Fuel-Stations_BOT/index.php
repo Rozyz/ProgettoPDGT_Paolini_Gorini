d52ca21f242068d77a27c87ca1fc89e8ad51309a
Rozyz <paolettopaolini@yahoo.it>
2019-04-02 09:31:38 +0200

    Correzioni generali

--- Fuel-Stations_BOT/index.php
+++ Fuel-Stations_BOT/index.php
@@ -2,6 +2,10 @@
   require_once(dirname(__FILE__).'/token.php');
   require_once(dirname(__FILE__).'/curl-lib.php');
 
+  $stato = [];
+
+  while(1){
+
   $last_update_filename = dirname(__FILE__) . '/last-update-id.txt';
   if(file_exists($last_update_filename)) {
     $last_update = intval(@file_get_contents($last_update_filename));
@@ -11,18 +15,15 @@
   }
 
   $website = "https://api.telegram.org/bot".$botToken;
-  $update = file_get_contents($website."/getupdates?offset=". ($last_update + 1) . "&limit=1");
-  //$update = file_get_contents("php://input");
-  $dati = json_decode($update, TRUE);
-  $stato = [];
+  $update = http_request($website."/getupdates?offset=". ($last_update + 1) . "&limit=1");
 
-  print_r($dati);
+  print_r($update);
 
-  if(isset($dati['result'][0])){
-    $update_id = $dati['result'][0]['update_id'];
-    $chat_id = $dati['result'][0]['message']['from']['id'];
-    $name = $dati['result'][0]['message']['from']['first_name'];
-    $text = $dati['result'][0]['message']['text'];
+  if(isset($update['result'][0])){
+    $update_id = $update['result'][0]['update_id'];
+    $chat_id = $update['result'][0]['message']['from']['id'];
+    $name = $update['result'][0]['message']['from']['first_name'];
+    $text = $update['result'][0]['message']['text'];
 
     echo "Chat_id: $chat_id\n";
     echo "Name: $name\n";
@@ -33,20 +34,63 @@
         case "/start":
           $msg = "Ciao $name e benvenuto!\nEcco a te la lista dei comandi disponibili su questo bot.
                  \n/stazione\n/add";
-          file_get_contents($website."/sendmessage?chat_id=".$chat_id."&text=".urlencode($msg)."");
+          http_request($website."/sendmessage?chat_id=".$chat_id."&text=".urlencode($msg)."");
           break;
+
         case "/stazione":
           $msg2 = "Inserisci il nome del comune del quale vuoi conoscere i benzinai disponibili\n";
-          file_get_contents($website."/sendmessage?chat_id=".$chat_id."&text=".urlencode($msg2)."");
+          http_request($website."/sendmessage?chat_id=".$chat_id."&text=".urlencode($msg2)."");
           $stato[(string)$chat_id] = 1;
+          break;
+
+        case "/goro":
+          $infos = http_request("https://fuel-stations-italy.herokuapp.com/comune/".$text."");
+          //print_r($info);
+          $i = 0;
+          $contatore = 0;
+          $stringa = "";
+          foreach($infos as $info){
+            $comune[$i] = $info['ccomune'];
+            $nome[$i] = $info['cnome'];
+            $provincia[$i] = $info['cprovincia'];
+            $i++;
+            $contatore++;
+          }
+          while($i != 0){
+            $i--;
+            if($nome[$i] == '')
+              $nome[$i] = "/";
+            $stringa = $stringa."Nome: {$nome[$i]}\n";
+
+          }
+          http_request($website."/sendmessage?chat_id=".$chat_id."&text=A ".$comune[0]." ci sono ".$contatore." stazioni e sono: ".urlencode($stringa)."");
           break;
 
         default:
           $infoMsg = "Digita un comando valido, coglione";
-          file_get_contents($website."/sendmessage?chat_id=".$chat_id."&text=".urlencode($infoMsg)."");
+          http_request($website."/sendmessage?chat_id=".$chat_id."&text=".urlencode($infoMsg)."");
       }
     }else if($stato[(string)$chat_id] == 1){
-      file_get_contents($website."/sendmessage?chat_id=".$chat_id."&text=Gianfranco");
+      $infos = http_request("https://fuel-stations-italy.herokuapp.com/comune/".$text."");
+      //print_r($info);
+      $i = 0;
+      $contatore = 0;
+      $stringa = "";
+      foreach($infos as $info){
+        $comune[$i] = $info['ccomune'];
+        $nome[$i] = $info['cnome'];
+        $provincia[$i] = $info['cprovincia'];
+        $i++;
+        $contatore++;
+      }
+      while($i != 0){
+        $i--;
+        if($nome[$i] == '')
+          $nome[$i] = "/";
+        $stringa = $stringa."Nome: {$nome[$i]}\n";
+
+      }
+      http_request($website."/sendmessage?chat_id=".$chat_id."&text=A ".$comune[0]." ci sono ".$contatore." stazioni e sono: ".urlencode($stringa)."");
       $stato[(string)$chat_id] = 0;
     }
     file_put_contents($last_update_filename, $update_id);
