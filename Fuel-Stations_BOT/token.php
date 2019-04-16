b1069b03acdfd4450554257a5c961eaaeb2a7db3
Paolo Paolini <paolettopaolini@yahoo.it>
2019-04-16 12:04:42 +0200

    Modifica

--- Fuel-Stations_BOT/token.php
+++ Fuel-Stations_BOT/token.php
@@ -1,5 +1,5 @@
 <?php
-  $botToken = "880821062:AAFtPv3MaFBKLrXNC5KmJJUiJxAbRLOjz0o";
+  $botToken = "862622038:AAGCzyPr0PYHKQT7F_xG9_xytTspG7nAEBA";
 
   $google_key = "AIzaSyCtNEV0lStU80DF9xGOx0GfX30WFA0qZtY";
  ?>
